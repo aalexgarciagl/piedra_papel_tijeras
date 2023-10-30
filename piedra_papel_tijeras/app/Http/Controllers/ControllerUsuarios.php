c53efff86d378763bce80ca45fed58c4708c2cf6
Alejandro <alex.garcia.2002@hotmail.com>
2023-10-30 10:27:21 +0100

    getUsers

--- piedra_papel_tijeras/app/Http/Controllers/ControllerUsuarios.php
+++ piedra_papel_tijeras/app/Http/Controllers/ControllerUsuarios.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuarios;
 use Illuminate\Http\Request;
 
 class ControllerUsuarios extends Controller
 {
     function getUsers(){
-        
+        $usuarios = Usuarios::all(); 
+        return response()->json($usuarios); 
     }
 }

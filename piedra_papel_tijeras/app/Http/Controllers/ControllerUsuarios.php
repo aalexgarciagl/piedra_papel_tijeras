a1f4772eb7dc1fba192dfadfa1e62f9495b27f5f
Alejandro <alex.garcia.2002@hotmail.com>
2023-10-30 10:44:03 +0100

    Update ControllerUsuarios.php
    setUser

--- piedra_papel_tijeras/app/Http/Controllers/ControllerUsuarios.php
+++ piedra_papel_tijeras/app/Http/Controllers/ControllerUsuarios.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Usuarios;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ControllerUsuarios extends Controller
 {
@@ -15,5 +16,15 @@ class ControllerUsuarios extends Controller
     function getUser($id){
         $usuario = Usuarios::find($id); 
         return response()->json($usuario); 
+    }
+
+    function setUser(Request $request){
+        $newUser = new Usuarios(); 
+        $newUser ->nombre = $request->get("nombre"); 
+        $newUser ->password = Hash::make($request->password); 
+        $newUser ->partidas_jugadas = 0; 
+        $newUser ->partidas_ganadas = 0;     
+        $newUser ->rol = 0;
+        $newUser->save();   
     }
 }
